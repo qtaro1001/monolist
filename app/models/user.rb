41ca0c5f9d8716a7a504d99281233aca20ff2297
qtaro1001 <yosoyokuno+cloud9@gmail.com>
2015-10-31 13:54:51 +0000

    add want have

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,10 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +34,26 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.create(user_id: item_id)
   end
 
   def unhave(item)
+    haves.find_by(user_id: item_id).destroy
   end
 
   def have?(item)
+    haves.include?(item)
   end
 
   def want(item)
+    wants.create(user_id: item_id)
   end
 
   def unwant(item)
+    wants.find_by(user_id: item_id).destroy
   end
 
   def want?(item)
+    wants.include?(items)
   end
 end
