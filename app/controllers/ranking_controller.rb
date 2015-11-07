d3b9c39cc948d67516df44c12e8e6cc6e75c4db4
qtaro1001 <yosoyokuno+cloud9@gmail.com>
2015-11-07 18:53:06 +0000

    Modify ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,11 @@
 class RankingController < ApplicationController
   def have
     @haves = Ownership.where(type: "Have").group("item_id").order('count_all desc').count
+    @haves = @haves.take(10)
   end
 
   def want
     @wants = Ownership.where(type: "Want").group("item_id").order('count_all desc').count
+    @wants = @wants.take(10)
   end
 end
