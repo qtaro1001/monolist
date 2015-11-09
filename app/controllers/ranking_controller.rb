ddcbe633dadb8c0f51ee816201e3344ba8cce0bb
qtaro1001 <yosoyokuno+cloud9@gmail.com>
2015-11-09 14:36:54 +0000

    modify ranking-controller

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,13 +1,19 @@
 class RankingController < ApplicationController
   
 def have
-  @ranking = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+  @ranking = Have.group(:item_id)
+                 .order('count_item_id desc')
+                 .limit(10)
+                 .count(:item_id)
   ids = @ranking.keys  #Keysメソッド…ハッシュのキーを集めて配列にして返す。
   @items = Item.find(ids).sort_by{|i| ids.index(i.id)}
 end
 
 def want
-  @ranking = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+  @ranking = Want.group(:item_id)
+                 .order('count_item_id desc')
+                 .limit(10)
+                 .count(:item_id)
   ids = @ranking.keys
   @items = Item.find(ids).sort_by{|i| ids.index(i.id)}
 end
